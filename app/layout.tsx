a66ea2e70e7f3f3196e95e4e928857555cd7f99b
Pranay <pranaygumashta@gmail.com>
2026-09-24 11:41:32 +0530

    Error Fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "WeatherGPT - ChatGPT Plugin to get the weather of any given location",
-  description:
+  description: 
     "WeatherGPT is a ChatGPT Plugin to get the weather of any given location. Built with Next.js and served from Vercel's Edge Network.",
   twitter: {
     card: "summary_large_image",
