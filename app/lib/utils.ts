45a249aaa6c76a99442a3de479baa4af67c02a92
Pranay <pranaygumashta@gmail.com>
2026-09-24 11:38:35 +0530

    Minor Changes

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -3,4 +3,4 @@ export async function getWeatherData(location: string) {
     `http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=${location}&aqi=no`,
     { next: { revalidate: 60 } }
   ).then((res) => res.json());
-}
+} 
